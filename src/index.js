bddb941ce809d5f2f846ba0b97d00014ca546a5d
Joshua Sanderson <joshua@thisisthesandersons.co.uk>
2021-09-23 18:23:43 +0100

    Added old client code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Index from './view/Index.jsx'
-import './view/index.css'
 import * as serviceWorker from './serviceWorker'
+import '../node_modules/carbon-components/scss/globals/scss/styles.scss'
 
 ReactDOM.render(<Index />, document.getElementById('root'))
 
--- src/view/Index.jsx
+++ src/view/Index.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { useParams } from "react-router"
+import React, { Suspense } from 'react'
+const ChatConponent = React.lazy(() => import('./Chat.jsx'))
+const DashboardIndex = React.lazy(() => import('./DashboardIndex.jsx'))
 
-class Index extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return <h1>Hello, World.</h1>
-  }
+function Chat() {
+    let { ChatName } = useParams()
+    var chatLink = "/chats/" + ChatName
+    return (< ChatConponent chatLink={chatLink} />)
 }
 
-export default Index;
+function Video() {
+    let chatName = '/live/stream.flv'
+    return (< Player videoLink={chatName} />)
+}
+
+function App() {
+    return (
+        <Suspense fallback={<div style={{ 'height': '100%', 'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center', 'padding': '10px' }}><div style={{ "padding": "10px", "margin": "10px", "backgroundClip": "padding-box", "backgroundColor": "#FFFFFF", "height": "100%", 'textAlign': 'center', 'fontFamily': 'Candara' }}>Loading....</div></div>}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" children={<div>Salesian chat rooms. Written by <a href="https://jsanderson.net">Joshua Sanderson</a></div>} />
+                    <Route path="/Dashboard" children={<DashboardIndex />} />
+                    <Route path="/:ChatName" children={<Chat />} />
+                </Switch>
+            </Router>
+        </Suspense>
+    );
+}
+
+export default App
